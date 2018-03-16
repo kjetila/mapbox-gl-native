d1af7204b412dbfcb489ab29aeb75e51e8f1b8ea
Kjetil <kjetil@ecc.no>
2018-03-16 15:14:56 +0100

    testing local 5.5.0

---
+++ platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/telemetry/HttpTransportTest.java
@@ -0,0 +1,19 @@
+package com.mapbox.mapboxsdk.telemetry;
+
+import org.junit.Test;
+
+import static com.mapbox.services.android.telemetry.utils.TelemetryUtils.toHumanReadableAscii;
+import static junit.framework.Assert.assertEquals;
+
+public class HttpTransportTest {
+
+  @Test
+  public void testNonAsciiUserAgent() {
+
+    final String swedishUserAgent = "Sveriges Fjäll/1.0/1 MapboxEventsAndroid/4.0.0-SNAPSHOT";
+    final String asciiVersion = "Sveriges Fj?ll/1.0/1 MapboxEventsAndroid/4.0.0-SNAPSHOT";
+
+    assertEquals("asciiVersion and swedishUserAgent should match", asciiVersion,
+      toHumanReadableAscii(swedishUserAgent));
+  }
+}
